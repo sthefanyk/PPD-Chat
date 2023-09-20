447172d5075aac64d845b47b950c873b4d54a828
sthefanyk <sthefanykmbrl@gmail.com>
2023-09-20 12:06:55 -0300

    modificações na tela de entrada

--- public/main.js
+++ public/main.js
@@ -1,115 +1,138 @@
+// Cria uma instância do socket.io para comunicação via WebSocket.
 const socket = io();
 
+// Variáveis para armazenar o nome de usuário e a lista de usuários conectados.
 let username = '';
 let userList = [];
 
+// Seleciona elementos HTML relevantes pelo ID.
+let joinButton = document.getElementById('joinButton');
+let roomSelect = document.getElementById('room');
 let loginPage = document.querySelector('#loginPage');
 let chatPage = document.querySelector('#chatPage');
 let loginInput = document.querySelector('#loginNameInput');
-let textInput= document.querySelector('#chatTextInput');
+let textInput = document.querySelector('#chatTextInput');
 
+// Define a exibição inicial das páginas.
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
 
+// Função para renderizar a lista de usuários na interface.
 function renderUserList() {
-    let ul =  document.querySelector('.userList');
+    let ul = document.querySelector('.userList');
     ul.innerHTML = '';
 
+    // Percorre a lista de usuários e adiciona cada um como um item de lista HTML.
     userList.forEach(i => {
         ul.innerHTML += '<li>' + i + '</li>';
     });
 }
 
+// Função para adicionar uma mensagem à janela de chat.
 function addMessage(type, user, msg) {
-    let ul =  document.querySelector('.chatList');
-    
+    let ul = document.querySelector('.chatList');
+
+    // Com base no tipo de mensagem (status ou mensagem de usuário), cria elementos de lista HTML.
     switch (type) {
         case 'status':
             ul.innerHTML += '<li class="m-status">' + msg + '</li>';
             break;
-    
         case 'msg':
             if (username == user) {
-                ul.innerHTML += '<li class="m-txt"><span class="me">' + user + '</span>' + msg + '</li>';
-            }else{
-                ul.innerHTML += '<li class="m-txt"><span>' + user + '</span>' + msg + '</li>';
+                ul.innerHTML += '<li class="m-txt"><span class="me">' + user + '</span> ' + msg + '</li>';
+            } else {
+                ul.innerHTML += '<li class="m-txt"><span>' + user + '</span> ' + msg + '</li>';
             }
             break;
     }
 
+    // Rola a janela de chat para a última mensagem.
     ul.scrollTop = ul.scrollHeight;
 }
 
-loginInput.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        let name = loginInput.value.trim();
-        if (name != '') {
-            username = name;
-            document.title = 'Chat (' + username + ')';
-            socket.emit('join-request', username);
-        }
+// Event listener para o campo de entrada de nome de usuário no formulário de login.
+joinButton.addEventListener('click', () => {
+    let name = loginInput.value.trim();
+    let selectedRoom = roomSelect.value;
+
+    if (name != '') {
+        username = name;
+        document.title = 'Chat (' + username + ')';
+
+        // Envia uma solicitação para ingressar no chat com o nome de usuário escolhido.
+        socket.emit('join-request', username, selectedRoom);
     }
+    
 });
 
+// Event listener para o campo de entrada de texto na janela de chat.
 textInput.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         let txt = textInput.value.trim();
+        textInput.value = '';
+
         if (txt != '') {
-            username = name;
+            // Adiciona a mensagem à janela de chat e a envia via WebSocket.
             addMessage('msg', username, txt);
             socket.emit('send-msg', txt);
         }
     }
 });
 
-socket.on('user-ok', (list) => {
+// Eventos socket.io para lidar com mensagens do servidor.
+
+// Quando o servidor confirma que o usuário se conectou com sucesso.
+socket.on('user-ok', (connectedUsers, selectedRoom) => {
     loginPage.style.display = 'none';
     chatPage.style.display = 'flex';
     textInput.focus();
 
-    addMessage('status', null, 'Conectado!');
+    // Adiciona uma mensagem de status à janela de chat.
+    addMessage('status', null, 'Conectado à sala: ' + selectedRoom);
 
-    userList = list;
-
+    // Atualiza a lista de usuários e a exibe na interface.
+    userList = connectedUsers;
     renderUserList();
 });
 
+// Quando o servidor envia uma atualização da lista de usuários.
 socket.on('list-update', (data) => {
-
     if (data.joined) {
-        addMessage('status', null, data.joined + 'entrou no chat.');
+        addMessage('status', null, data.joined + ' entrou no chat.');
     }
 
     if (data.left) {
-        addMessage('status', null, data.left + 'saiu do chat.');
+        addMessage('status', null, data.left + ' saiu do chat.');
     }
 
+    // Atualiza a lista de usuários e a exibe na interface.
     userList = data.list;
     renderUserList();
 });
 
+// Quando o servidor envia uma mensagem de chat.
 socket.on('show-msg', (data) => {
+    // Adiciona a mensagem à janela de chat.
     addMessage('msg', data.username, data.message);
 });
 
+// Quando a conexão com o servidor é encerrada.
 socket.on('disconnect', () => {
     addMessage('status', null, 'Você foi desconectado!');
     userList = [];
     renderUserList();
 });
 
+// Quando ocorre um erro de reconexão com o servidor.
 socket.on('reconnect_error', () => {
     addMessage('status', null, 'Tentando reconectar...');
 });
 
+// Quando a reconexão com o servidor é bem-sucedida.
 socket.on('reconnect', () => {
     addMessage('status', null, 'Reconectado!');
+
     if (username != '') {
-        socket.emit('join-request', username);
+        socket.emit('join-request', username, roomSelect.value);
     }
 });
-
-
-
-
-
--- server.js
+++ server.js
@@ -3,46 +3,47 @@ const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const app = express(); // Cria uma instância do Express.js
+const server = http.createServer(app); // Cria um servidor HTTP usando o Express.js
+const io = socketIO(server); // Cria uma instância do Socket.io vinculada ao servidor HTTP
 
-server.listen(3000);
-app.use(express.static(path.join(__dirname, 'public')));
+server.listen(3000); // O servidor está ouvindo na porta 3000
 
-let connectedUsers = [];
+app.use(express.static(path.join(__dirname, 'public'))); // Serve arquivos estáticos a partir do diretório 'public'
+
+let connectedUsers = []; // Inicializa um array vazio para rastrear os usuários conectados
 
 io.on('connection', (socket) => {
-    console.log("Conexão detectada...");
+    console.log("Conexão detectada..."); // Exibe uma mensagem no servidor quando um cliente se conecta via WebSocket
 
     socket.on('join-request', (username) => {
-        socket.username = username;
-        connectedUsers.push(username);
-        console.log(connectedUsers);
+        socket.username = username; // Define o nome de usuário para o socket
+        connectedUsers.push(username); // Adiciona o nome de usuário à lista de usuários conectados
+        console.log(connectedUsers); // Exibe a lista de usuários conectados no servidor
 
-        socket.emit('user-ok'. connectedUsers);
+        socket.emit('user-ok', connectedUsers); // Envia a lista de usuários conectados para o cliente
         socket.broadcast.emit('list-update', {
             joined: username,
             list: connectedUsers
-        });
+        }); // Envia uma mensagem para todos os outros clientes informando que um novo usuário se juntou à sala
     });
 
     socket.on('disconnect', () => {
-        connectedUsers = connectedUsers.filter(u => u != socket.username);
-        console.log(connectedUsers);
-        
+        connectedUsers = connectedUsers.filter(u => u != socket.username); // Remove o usuário desconectado da lista de usuários conectados
+        console.log(connectedUsers); // Exibe a lista atualizada de usuários conectados no servidor
+
         socket.broadcast.emit('list-update', {
             left: socket.username,
             list: connectedUsers
-        });
+        }); // Informa aos outros clientes que um usuário desconectou
     });
 
     socket.on('send-msg', (txt) => {
         let obj = {
             username: socket.username,
             message: txt
-        }
-        
-        socket.broadcast.emit('show-msg', obj);
+        };
+
+        socket.broadcast.emit('show-msg', obj); // Envia a mensagem para todos os outros clientes, exceto o remetente
     });
 });
